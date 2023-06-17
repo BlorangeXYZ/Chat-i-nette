cdf7813f7403146201936c3e4d1afe75dfb44edd
jsprenge <jsprenge@student.42wolfsburg.de>
2023-06-17 17:47:54 +0200

    add branding

--- App/pages/_document.tsx
+++ App/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head/>
-      <title>42 Assistant</title>
+      <title>Chat-I-nette</title>
       <body>
         <Main />
         <NextScript />
